0d66cf8ba300b912f4408a6b02f9f275d3d2c6c2
black-titan0 <alisafara@ce.sharif.edu>
2021-04-27 01:54:20 +0430

    All menues cleaned up

--- src/main/java/view/ConsoleBasedMenus.java
+++ src/main/java/view/ConsoleBasedMenus.java
@@ -16,36 +16,33 @@ public class ConsoleBasedMenus {
 
     private String runningMenu = "register";
 
-    private String[] REGISTER_MENUS_REGEXES = new String[7];
+    private final String[] registerMenusRegexes = {
+            "^user create (--username|-u) (?<username>\\w+) (--nickname|-n) (?<nickname>\\w+) (--password|-p) (?<password>\\w+)$",
+            "^user create (--username|-u) (?<username>\\w+) (--password|-p) (?<password>\\w+) (--nickname|-n) (?<nickname>\\w+)$",
+            "^user login (--username|-u) (?<username>\\w+) (--password|-p) (?<password>\\w+)$",
+            "^user login (--password|-p) (?<password>\\w+) (--username|-u) (?<username>\\w+)$",
+            "^menu enter (?<name>Main|Deck|Duel|Profile|Scoreboard|Shop)$",
+            "^menu show-current$",
+            "^menu exit$"
+    };
 
-    {
-        REGISTER_MENUS_REGEXES[0] = "^user create (--username|-u) (?<username>\\w+) (--nickname|-n) (?<nickname>\\w+) (--password|-p) (?<password>\\w+)$";
-        REGISTER_MENUS_REGEXES[1] = "^user create (--username|-u) (?<username>\\w+) (--password|-p) (?<password>\\w+) (--nickname|-n) (?<nickname>\\w+)$";
-        REGISTER_MENUS_REGEXES[2] = "^user login (--username|-u) (?<username>\\w+) (--password|-p) (?<password>\\w+)$";
-        REGISTER_MENUS_REGEXES[3] = "^user login (--password|-p) (?<password>\\w+) (--username|-u) (?<username>\\w+)$";
-        REGISTER_MENUS_REGEXES[4] = "^menu enter (?<name>Main|Deck|Duel|Profile|Scoreboard|Shop)$";
-        REGISTER_MENUS_REGEXES[5] = "^menu show-current$";
-        REGISTER_MENUS_REGEXES[6] = "^menu exit$";
-    }
+    private final String[] mainMenusRegexes = {
+            "^menu enter (?<name>Duel|Deck|Scoreboard|Profile|Shop)$",
+            "^user logout$",
+            "^menu show-current$",
+            "^menu exit$"
+    };
+    private final String[] SCORE_BOARD_REGEXES = {
 
-    private String[] MAIN_MENUS_REGEXES = new String[4];
+    };
 
-    {
-        MAIN_MENUS_REGEXES[0] = "^menu enter (?<name>Duel|Deck|Scoreboard|Profile|Shop)$";
-        MAIN_MENUS_REGEXES[1] = "^user logout$";
-        MAIN_MENUS_REGEXES[2] = "^menu show-current$";
-        MAIN_MENUS_REGEXES[3] = "^menu exit$";
-    }
-
-    private String[] PROFILE_MENUS_REGEXES = new String[5];
-
-    {
-        PROFILE_MENUS_REGEXES[0] = "^profile change (--nickname|-n) (?<nickname>\\w+)$";
-        PROFILE_MENUS_REGEXES[1] = "^profile change (--password|-p) --current (?<oldPass>\\w+) --new (?<newPass>\\w+)$";
-        PROFILE_MENUS_REGEXES[2] = "^profile change (--password|-p) --new (?<newPass>\\w+) --current (?<oldPass>\\w+)$";
-        PROFILE_MENUS_REGEXES[3] = "^menu show-current$";
-        PROFILE_MENUS_REGEXES[4] = "^menu exit$";
-    }
+    private final String[] profileMenusRegexes = {
+            "^profile change (--nickname|-n) (?<nickname>\\w+)$",
+            "^profile change (--password|-p) --current (?<oldPass>\\w+) --new (?<newPass>\\w+)$",
+            "^profile change (--password|-p) --new (?<newPass>\\w+) --current (?<oldPass>\\w+)$",
+            "^menu show-current$",
+            "^menu exit$"
+    };
 
     private ConsoleBasedMenus() {
     }
@@ -60,24 +57,20 @@ public class ConsoleBasedMenus {
 
 
     public void runRegisterMenu() {
-        boolean isMatchCommand = false;
-        Matcher commandMatcher = null;
+        Matcher commandMatcher;
         String command;
         while (runningMenu.equals("register")) {
             command = scanner.nextLine().replaceAll("\\s+", " ");
-
             int whichCommand;
-            for (whichCommand = 0; whichCommand < 6; whichCommand++) {
-                commandMatcher = findMatcher(command, REGISTER_MENUS_REGEXES[whichCommand]);
+            for (whichCommand = 0; whichCommand < 7; whichCommand++) {
+                commandMatcher = findMatcher(command, registerMenusRegexes[whichCommand]);
                 if (commandMatcher.find()) {
                     executeRegisterMenuCommands(commandMatcher, whichCommand);
                     break;
-                } else if (whichCommand == 5)
+                } else if (whichCommand == 6)
                     Output.getInstance().showMessage("invalid command");
             }
-
         }
-
     }
 
     private void executeRegisterMenuCommands(Matcher commandMatcher, int whichCommand) {
@@ -95,10 +88,11 @@ public class ConsoleBasedMenus {
                     if (!ErrorChecker.isUserLoggedIn())
                         Output.getInstance().showMessage("please login first");
                     else {
-                        runMainMenu();
                         runningMenu = "main";
+                        runMainMenu();
                     }
                 } else Output.getInstance().showMessage("menu navigation is not possible");
+                break;
             case 5:
                 Output.getInstance().showMessage("Register Menu");
                 break;
@@ -108,32 +102,53 @@ public class ConsoleBasedMenus {
     }
 
     public void runMainMenu() {
-        boolean isMatchCommand = false;
         Matcher commandMatcher;
         String command;
-        while (true) {
+        while (runningMenu.equals("main")) {
             command = scanner.nextLine().replaceAll("\\s+", " ");
+            int whichCommand;
+            for (whichCommand = 0; whichCommand < 4; whichCommand++) {
+                commandMatcher = findMatcher(command, mainMenusRegexes[whichCommand]);
+                if (commandMatcher.find()) {
+                    executeMainMenuCommands(commandMatcher, whichCommand);
+                    break;
+                } else if (whichCommand == 3)
+                    Output.getInstance().showMessage("invalid command");
+            }
+        }
+    }
 
-            commandMatcher = findMatcher(command, MAIN_MENUS_REGEXES[0]);
-            if (commandMatcher.find()) {
+    private void executeMainMenuCommands(Matcher commandMatcher, int whichCommand) {
+        switch (whichCommand) {
+            case 0:
                 String menuName = commandMatcher.group("name");
-                if (menuName.equals("Duel")) runDuelMenu();
-                if (menuName.equals("Deck")) runDeckMenu();
-                if (menuName.equals("Scoreboard")) runScoreboard();
-                if (menuName.equals("Profile")) runProfileMenu();
-                if (menuName.equals("Shop")) runShopping();
-                isMatchCommand = true;
-            }
-            commandMatcher = findMatcher(command, MAIN_MENUS_REGEXES[2]);
-            if (commandMatcher.find()) {
+                if (menuName.equals("Duel")) {
+                    runningMenu = "duel";
+                    runDuelMenu();
+                }
+                if (menuName.equals("Deck")) {
+                    runningMenu = "deck";
+                    runDeckMenu();
+                }
+                if (menuName.equals("Scoreboard")) {
+                    runningMenu = "scoreboard";
+                    runScoreboard();
+                }
+                if (menuName.equals("Profile")) {
+                    runningMenu = "profile";
+                    runProfileMenu();
+                }
+                if (menuName.equals("Shop")) {
+                    runningMenu = "shop";
+                    runShopping();
+                }
+                break;
+            case 3:
+            case 1:
+                runningMenu = "register";
+                break;
+            case 2:
                 Output.getInstance().showMessage("Main Menu");
-                isMatchCommand = true;
-            }
-            commandMatcher = findMatcher(command, MAIN_MENUS_REGEXES[1]);
-            if (commandMatcher.find()) return;
-            commandMatcher = findMatcher(command, MAIN_MENUS_REGEXES[3]);
-            if (commandMatcher.find()) return;
-            if (!isMatchCommand) Output.getInstance().showMessage("invalid command");
         }
     }
 
@@ -146,60 +161,62 @@ public class ConsoleBasedMenus {
     }
 
     public void runScoreboard() {
-        boolean isMatchCommand = false;
         String command;
-
-        while (true) {
+        while (runningMenu.equals("scoreboard")) {
             command = scanner.nextLine().replaceAll("\\s+", " ");
-            if (command.equals("scoreboard show")) {
-                Scoreboard.getInstance().showScoreboard();
-                isMatchCommand = true;
+            switch (command) {
+                case "scoreboard show":
+                    Scoreboard.getInstance().showScoreboard();
+                    break;
+                case "menu show-current":
+                    Output.getInstance().showMessage("Scoreboard Menu");
+                    break;
+                case "menu exit":
+                    runningMenu = "main";
+                    return;
+                default:
+                    Output.getInstance().showMessage("invalid command");
             }
-            if (command.equals("menu show-current")) {
-                Output.getInstance().showMessage("Scoreboard Menu");
-                isMatchCommand = true;
-            }
-            if (command.startsWith("menu enter")) {
-                Output.getInstance().showMessage("menu navigation is not possible");
-                isMatchCommand = true;
-            }
-            if (command.equals("menu exit")) return;
-            if (!isMatchCommand) Output.getInstance().showMessage("invalid command");
         }
     }
 
     public void runProfileMenu() {
-        Player playerLoggedIn = MainMenu.getInstance().getPlayerLoggedIn();
-        boolean isMatchCommand = false;
         Matcher commandMatcher;
         String command;
-        while (true) {
+        while (runningMenu.equals("profile")) {
             command = scanner.nextLine().replaceAll("\\s+", " ");
+            int whichCommand;
+            for (whichCommand = 0; whichCommand < 5; whichCommand++) {
+                commandMatcher = findMatcher(command, profileMenusRegexes[whichCommand]);
+                if (commandMatcher.find()) {
+                    executeProfileMenuCommands(commandMatcher, whichCommand);
+                    break;
+                } else if (whichCommand == 4)
+                    Output.getInstance().showMessage("invalid command");
+            }
 
-            commandMatcher = findMatcher(command, PROFILE_MENUS_REGEXES[0]);
-            if (commandMatcher.find()) {
+        }
+    }
+
+    private void executeProfileMenuCommands(Matcher commandMatcher, int whichCommand) {
+        Player playerLoggedIn = MainMenu.getInstance().getPlayerLoggedIn();
+        switch (whichCommand) {
+            case 0:
                 ProfileController.getInstance().changeNickname(playerLoggedIn, commandMatcher.group("nickname"));
-                isMatchCommand = true;
-            }
-            commandMatcher = findMatcher(command, PROFILE_MENUS_REGEXES[1]);
-            if (commandMatcher.find()) {
+                break;
+            case 1:
+            case 2:
                 String oldPass = commandMatcher.group("oldPass");
                 String newPass = commandMatcher.group("newPass");
                 ProfileController.getInstance().changePassword(playerLoggedIn, oldPass, newPass);
-                isMatchCommand = true;
-            }
-            commandMatcher = findMatcher(command, PROFILE_MENUS_REGEXES[2]);
-            if (commandMatcher.find()) {
-                String oldPass = commandMatcher.group("oldPass");
-                String newPass = commandMatcher.group("newPass");
-                ProfileController.getInstance().changePassword(playerLoggedIn, oldPass, newPass);
-                isMatchCommand = true;
-            }
-            commandMatcher = findMatcher(command, PROFILE_MENUS_REGEXES[3]);
-            if (commandMatcher.find()) Output.getInstance().showMessage("Profile Menu");
-            commandMatcher = findMatcher(command, PROFILE_MENUS_REGEXES[4]);
-            if (commandMatcher.find()) return;
-            if (!isMatchCommand) Output.getInstance().showMessage("invalid command");
+                break;
+
+            case 3:
+                Output.getInstance().showMessage("Profile Menu");
+                break;
+
+            case 4:
+                runningMenu = "main";
         }
     }
 
