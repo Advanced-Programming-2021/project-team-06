d6dab9e1213d0271488e8c9f19ee617a7069e9bc
soheilnazari2001 <s.nazari80820@gmail.com>
2021-06-25 13:38:00 +0430

    Test

--- src/test/java/EffectsTest.java
+++ src/test/java/EffectsTest.java
@@ -2,18 +2,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.PublicKey;
+import java.util.ArrayList;
 
 public class EffectsTest {
 
     @Test
     public void testMonsterRebornEffect() throws Exception {
-        Assertions.assertTrue(runTestNumber("1"));
+        ArrayList<String> result = runTestNumber("1");
+        Assertions.assertEquals(result.get(0) , result.get(1));
     }
-    private boolean runTestNumber(String number) throws Exception {
+    private ArrayList<String> runTestNumber(String number) throws Exception {
         String parentFile = "./src/test/resources/" + number +"/";
         String deckFile = Files.readString(Path.of(parentFile + "deck.txt")),
               inputFile = Files.readString(Path.of(parentFile + "input.txt")),
@@ -24,8 +27,9 @@ public class EffectsTest {
         ByteArrayInputStream in = new ByteArrayInputStream(inputFile.getBytes());
         System.setIn(in);
         Main.main(null);
-        String finalOutput = outContent.toString().replace("\n" , "");
-        String finalExpectedOutput = outputFile.replace("\n" , "");
-        return finalExpectedOutput.equals(finalOutput.toString());
+        ArrayList<String> result = new ArrayList<String>();
+        result.add(outputFile.replaceAll("\\s",""));
+        result.add(String.valueOf(outContent).replaceAll("\\s", ""));
+        return result;
     }
 }
